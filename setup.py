3c1843363de4eb88d2067f472e9ae06e2435d77b
liudehua <liudehua@123.cn>
2025-02-06 18:20:33 +0800

    new .

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="bloom_filter_redis",
+    name="bloom_hh_redis",
     version="0.1",
     packages=find_packages(),
     install_requires=[
